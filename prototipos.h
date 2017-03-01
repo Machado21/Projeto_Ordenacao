55422b94ff8f93f4faad52d2b8167ea7d5a1a691
pires28 <junior-pires.rr@hotmail.com>
2017-03-01 15:19:55 -0300

    Update prototipos.h

--- prototipos.h
+++ prototipos.h
@@ -6,4 +6,26 @@ void insertionSort(int vetor[], int tamanho);
 
 void radixsort(int vetor[], int tamanho);
 
-void geraArquivo(int numeros[], int quantNumeros, int limite);
+void quick(int vet[], int inicio, int fim);
+
+int particiona( vet , inicio , fim);
+
+void mergeSort(int *vetor, int posicaoInicio, int posicaoFim);
+
+int buscaLinear(char vetor[], int tamanho, char elementoProcurado);
+
+int PesquisaBinaria ( int vet[], int chave, int Tam);
+
+void heapsort(int a[], int n);
+
+//void geraArquivo(int numeros[], int quantNumeros, int limite);
+
+void geraArquivo();
+
+void geraNumeros(int numeros[], int quantNumeros, int limite);
+
+int existe(int valores[], int tamanho, int valor);
+
+void arquivoOrdenadoBubbleSort100();
+
+void arquivoOrdenadoBubbleSort1000();
